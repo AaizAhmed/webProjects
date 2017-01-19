b73760b6d3192f9c8f6121c95fccb14201984164
AAIZAHMED <aaiza2@umbc.edu>
2017-01-19 18:23:26 -0500

    done!

--- Local_Weather_App/weather.js
+++ Local_Weather_App/weather.js
@@ -109,227 +109,13 @@ function getWeather(data)
 	}
 
 	//Adding weather details  thunderstorm with light drizzle
-	$("#desc").text( "thunderstorm with heavy drizzle" );
-	$("#wind").text( "Wind " + data["wind"]["speed"] + " mph" );
-	$("#humi").text( "Humidity " + data.main.humidity + " %");
-}
-
-function setBackground(pick)
-{
-	console.log(pick);
-	/*
-    	Backgrounds
-    	1. Thunderstorm
-    	2. Drizzle
-    	3. Rain
-    	4. Snow
-    	5. Atmosphare
-    	6. Clear
-    	7. Clouds
-    	8. Extreme
-    */
-    switch (pick)
-    {
-    	case 'Clouds': 
-    	{
-    		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/OverCast.jpg')";
-    		break;
-    	}
-    	case 'Thunderstorm': 
-    	{
-    		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/ThunderstormSkyTrees.jpg')";
-    		break;
-    	}
-    	case 'Rain': 
-    	{
-    		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/RainDropsSplashes.jpg')";
-    		break;
-    	}
-    	case 'Clear': 
-    	{
-    		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/RainbowArch.jpg')";
-    		break;
-    	}
-    	case 'Snow': 
-    	{
-    		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/SnowSnowflak.jpg')";
-    		break;
-    	}
-    	case 'Extreme': 
-    	{
-    		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/Tornadoes.jpg')";
-    		break;
-    	}
-    }
-}
-
-function convertTemp()
-{
-	var temp = document.getElementById("tempVal").innerHTML;
-
-	if ($( "input[name='F/C']:checked" ).val() == 'C')
-	{
-		temp = Math.round( (temp - 32) * 5/9 );
-	}
-	else
-	{
-		temp = Math.round( (temp * 9/5) + 32 );
-	}
-
-	if (!isNaN(temp)) 	
-	{	$("#tempVal").text(temp);	}
-}
-
-function error() 
-{
-    alert("Unable to retrieve your location");
-}
-
-$("document").ready (function ()
-{   
-  if (window.location.protocol != "https:")
-   {
-            var msg = "For Current Location you need to use HTTPS.<br> Click here: ";
-                msg += "<a target='_blank' href='https://codepen.io/AaizAhmed/full/PWbKNY/'>Local Weather</a>";
-
-          $("#httpError").html(msg);
-            defaultWeather();
-        } 
-      else 
-      { 
-            getLocation(); 
-      }
-
-} );/*
-	Author: Aaiz Ahmed
-	Date: January 17, 2017
-*/
-
-function defaultWeather ()
-{
-	var key = '&APPID=e579c8ec4536594e206ab2b517e975af';    //D.C. ID: 4140963
-    var url = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?id=4140963&units=imperial';
-    url += key;
-    
-    $.getJSON(url, getDefaultWeather); 
-  
-  return true;
-}
-    
-function getDefaultWeather(data)	
-{
-	//Adding background image.
-	var catogary = data["weather"][0]["main"];
-	setBackground (catogary);
-
-    //Adding Location
-    $("#location").text(data["name"] + ', ' + data["sys"]["country"]);
-	
-	//Adding Temperature
-	var temp = Math.round( data["main"]["temp"] );
-	$("#tempVal").text(temp);
-
-	//Adding Icon
-	var iconUrl = 'http://openweathermap.org/img/w/' + data["weather"][0]["icon"] + '.png';
-	var addIcon = '<img id="img1" src="' + iconUrl + '">';
-	$("#weatherIcon").append(addIcon);
-
-	//Adding details
-	var arr =  data["weather"][0]["description"].split(' ');
-	var finalDesc = '';
-	for (var i = 0; i < arr.length; i++)
-	{
-		finalDesc += arr[i][0].toUpperCase() + arr[i].substring(1, arr[i].length) + ' ';  
-	}
-
 	$("#desc").text( finalDesc );
-	$("#wind").append( data["wind"]["speed"] + " mph");
-	$("#humi").append( data.main.humidity + " %");
-  
-  return true;
-} 
-
-function getLocation()
-{
-  if(navigator.geolocation)
-	{
-		navigator.geolocation.getCurrentPosition(getValues, error);
-	}
-	else
-	{
-		alert("Geolocation is not supported by your browser");
-	}
-}
-
-function getValues(position)
-{
-	var lati  = position.coords.latitude;
-    var longi = position.coords.longitude;
-
-	//Getting location from Google maps to include information like State and Zip code.
-    var urlLocation = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lati + ',' + longi + '&sensor=true';
-
-    $.getJSON 
-    ( urlLocation, function(data) 
-	    {
-	    	//Adding Location
-	      $('#location').text(data.results[1].formatted_address); 
-			  //console.log(data.results[1].formatted_address);
-	    }
-	); 
-
-	var key = '&APPID=e579c8ec4536594e206ab2b517e975af';    
-    var url = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?';    
-    url += 'lat=' + lati + '&lon=' + longi + '&units=imperial' + key;
-   
-    $.getJSON(url, getWeather);   
-}
-
-function getWeather(data)
-{
-	//Adding background image.
-	var catogary = data["weather"][0]["main"];
-	setBackground (catogary);
-    
-	
-	//Adding Temperature
-	var temp = Math.round( data["main"]["temp"] );
-	$("#tempVal").text(temp);
-
-	//Adding Icon
-	var iconUrl = 'http://openweathermap.org/img/w/' + data["weather"][0]["icon"] + '.png';
-	var addIcon = '<img id="img1" src="' + iconUrl + '">';
-	
-	$("#weatherIcon").html(addIcon);
-
-	//Capatalizing 1st letter of words in description.
-	var arr =  data["weather"][0]["description"].split(' ');
-	var finalDesc = '';
-	for (var i = 0; i < arr.length; i++)
-	{
-		finalDesc += arr[i][0].toUpperCase() + arr[i].substring(1, arr[i].length) + ' ';  
-	}
-
-	//Adding weather details  thunderstorm with light drizzle
-	$("#desc").text( "thunderstorm with heavy drizzle" );
 	$("#wind").text( "Wind " + data["wind"]["speed"] + " mph" );
 	$("#humi").text( "Humidity " + data.main.humidity + " %");
 }
 
 function setBackground(pick)
 {
-	console.log(pick);
-	/*
-    	Backgrounds
-    	1. Thunderstorm
-    	2. Drizzle
-    	3. Rain
-    	4. Snow
-    	5. Atmosphare
-    	6. Clear
-    	7. Clouds
-    	8. Extreme
-    */
     switch (pick)
     {
     	case 'Clouds': 
@@ -340,12 +126,28 @@ function setBackground(pick)
     	case 'Thunderstorm': 
     	{
     		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/ThunderstormSkyTrees.jpg')";
-    		break;
+    		
+        //Changing weather display properties
+        $("#geoInfo").css('color', 'white');
+       
+        $("#temp").css('background-color', 'white');
+        $("#details").css('background-color', 'white');
+       
+        $("#temp").css('border-color', 'orange');
+        $("#details").css('border-color', 'orange');
+       
+        break;
     	}
     	case 'Rain': 
     	{
     		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/RainDropsSplashes.jpg')";
-    		break;
+    		
+        //Changing weather display properties FF7F00 = orange
+       $("#geoInfo").css('color', '	#FF7F00');
+       $("#temp").css('color', '	#FF7F00');
+       $("#details").css('color', '	#FF7F00');
+
+        break;
     	}
     	case 'Clear': 
     	{
@@ -355,12 +157,24 @@ function setBackground(pick)
     	case 'Snow': 
     	{
     		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/SnowSnowflak.jpg')";
-    		break;
+    		
+        //Changing weather display properties
+       $("#temp").css('color', 'black');
+       $("#details").css('color', 'black');
+        
+        break;
     	}
     	case 'Extreme': 
     	{
     		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AaizAhmed/Images/master/Tornadoes.jpg')";
-    		break;
+    		
+        //Changing weather display properties
+       $("#geoInfo").css('color', 'blue');
+        
+       $("#temp").css('color', 'red');
+       $("#details").css('color', 'red');    
+        
+        break;
     	}
     }
 }
@@ -386,6 +200,7 @@ function error()
 {
     alert("Unable to retrieve your location");
 }
+
 
 // $("document").ready (function ()
 // {   
