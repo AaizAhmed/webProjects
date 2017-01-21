edc0fb490a7985b636980fad9b74507a9c2c356d
Aaiz Ahmed <aaiza2@umbc.edu>
2017-01-21 01:30:45 -0500

    done!

--- Wikipedia_Viewer/wikipedia.js
+++ Wikipedia_Viewer/wikipedia.js
@@ -2,35 +2,45 @@
 /*
 	Author: Aaiz N Ahmed
 	Data: January 20, 2017
-
-	Random: https://en.wikipedia.org/wiki/Special:Random
 */
 
 function getInput ()
 {
-	var word = document.getElementById('inputBox').value;
-	console.log(word);	
+	var word = document.getElementById('inputBox').value; 
 
-	var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=';
-	wikiUrl += word + '&limit=15&namespace=0&format=json';
+	var wikiUrl = 'http://www.wikipedia.org/w/api.php?action=opensearch&search=';
+	wikiUrl += word + '&limit=15&namespace=0&callback=getArticles';
 
-	$http.jsonp(wikiUrl).success(getArticles) ;
+	var tag = document.createElement("script");
+	tag.src = wikiUrl;
+
+	//Adding JS to the HTML header
+	document.getElementsByTagName("head")[0].appendChild(tag);
 }
 
 function getArticles(data)
 {
-	//Sucess GETTING URLs and names now!!
-
-	// data[3][0];
-
+	//Moving button and text box to display articles.
 	$("#randomArticle").css('margin-top', '1%');
 	$("#box").css('margin-top', '1%');
-	console.log(data[3].length);	
+
+	var titles = data[1];
+	var desc = data[2];
+	var urls = data[3];
+
+	for(i in urls)
+	{
+		var display = '<a href="' + urls[i] + '" target="_blank">';
+		display += '<div class="makeBox"><h4>' + titles[i] + '</h4><br>' + desc[i] + '</div>';
+		display += '</a>';
+
+		$('#displayWiki').append(display);
+	}
 }
 
-$(document).keyup(function (e) 
+$(document).keyup(function (event) 
 {
-    if( $(".inputBox:focus") && (e.keyCode === 13)) 
+    if( $(".inputBox:focus") && (event.keyCode === 13)) 
     {
        getInput();
     }
