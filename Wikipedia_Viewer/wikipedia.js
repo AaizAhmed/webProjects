a5025446c1de76b364c29186a1c7d1e78482b93c
Aaiz Ahmed <aaiza2@umbc.edu>
2017-01-20 21:33:26 -0500

    getting data

--- Wikipedia_Viewer/wikipedia.js
+++ Wikipedia_Viewer/wikipedia.js
@@ -5,16 +5,33 @@
 
 	Random: https://en.wikipedia.org/wiki/Special:Random
 */
+
 function getInput ()
 {
-	$("#inputBox").keyup( function(event) 
-	{
-	    if(event.keyCode == 13)
-	    {
-	    	//CAll the method, get the wikipedia articles
-	        //$("#id_of_button").click();
+	var word = document.getElementById('inputBox').value;
+	console.log(word);	
 
-	        console.log(document.getElementById('inputBox').value);
-	    }
-	});
+	var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=';
+	wikiUrl += word + '&limit=15&namespace=0&format=json';
+
+	$http.jsonp(wikiUrl).success(getArticles) ;
 }
+
+function getArticles(data)
+{
+	//Sucess GETTING URLs and names now!!
+
+	// data[3][0];
+
+	$("#randomArticle").css('margin-top', '1%');
+	$("#box").css('margin-top', '1%');
+	console.log(data[3].length);	
+}
+
+$(document).keyup(function (e) 
+{
+    if( $(".inputBox:focus") && (e.keyCode === 13)) 
+    {
+       getInput();
+    }
+ });
