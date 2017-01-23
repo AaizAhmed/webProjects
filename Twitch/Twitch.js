a3946d282e2c51e84f301c4fbe757292d4f09898
AAIZAHMED <aaiza2@umbc.edu>
2017-01-23 15:59:17 -0500

    logic done

---
+++ Twitch/Twitch.js
@@ -0,0 +1,68 @@
+/*
+	Aaiz N Ahmed
+	January 23, 2017
+	https://wind-bow.gomix.me/twitch-api
+*/
+
+function getData()
+{
+	var channelList = ["freecodecamp","ESL_SC2"];
+					  
+
+	for (i in channelList)
+	{
+		var statusUrl = 'https://wind-bow.gomix.me/twitch-api/streams/';
+	    statusUrl += channelList[i] + '?callback=getStatus';
+
+
+	    tag = document.createElement("script");
+		tag.src = statusUrl;
+		document.getElementsByTagName("head")[0].appendChild(tag);	
+	}
+}
+
+function useChannelUrl(data)
+{	
+	var channelName = data["name"];		
+	var div = '<div id=' + channelName + '>' 
+	var img = '<img src="' + data["logo"] + '" width="50px" height="50px">';
+
+	div += img + channelName + ' ' + 'Offline' + '</div>';
+
+	$("#offline").append(div);
+	console.log(data);
+}
+
+function getStatus(data)
+{
+	if (data.stream == null)
+	{
+		var tmp = data["_links"]["channel"];
+		var name = tmp.substring(38, tmp.length);
+
+		var channelUrl = "https://wind-bow.gomix.me/twitch-api/channels/";
+		channelUrl += name + '?callback=useChannelUrl';
+
+		var tag = document.createElement("script");
+		tag.src = channelUrl;
+		document.getElementsByTagName("head")[0].appendChild(tag);	
+
+	}
+	else
+	{
+		var channelName = data["stream"]["channel"]["display_name"];		
+		var div = '<div id=' + channelName + '>' 
+		var logo = data["stream"]["channel"]["logo"];
+		var gameDesc = data["stream"]["channel"]["game"] + ': ' + data["stream"]["channel"]["status"];
+
+		var img = '<img src="' + logo + '" width="50px" height="50px"> \t\t\t';
+		
+		div += img + channelName + '\t\t\t' + gameDesc + '</div>';
+
+		$("#online").append(div);
+
+		console.log(data);
+	}
+}
+
+getData();
