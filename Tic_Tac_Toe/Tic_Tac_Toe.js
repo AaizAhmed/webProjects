ab46b89b1537753e6ba475e7b7094a3a03b088a3
Aaiz Ahmed <aaiza2@umbc.edu>
2017-03-22 01:55:00 -0400

    click working

--- Tic_Tac_Toe/Tic_Tac_Toe.js
+++ Tic_Tac_Toe/Tic_Tac_Toe.js
@@ -263,6 +263,10 @@ var Game = function (board)
 
 function play_game()
 {
+	id = event.target.id;
+
+	display(id);
+
 	var board_obj = new Tic_Tac_Toe_Board();
 	var board = board_obj.getBoard();
 
@@ -287,8 +291,6 @@ function play_game()
 	ai.findBestMove(my_board_3);
 }
 
-// play_game();
-
 function addBoard(sign)
 {
 
@@ -312,139 +314,20 @@ function addBoard(sign)
       $("#turn").text("Computer's Turn");
    }
 
-   $('td').on('click', play_game);
-   // {
-   // 		id = event.target.id;
-   // });
-
-   // console.log(id);
-
-   play_game();
+   // Add the event trigger
+   $('td').on( 'click', play_game );
  
-}
-
-function checkWin()
-{
-	for (var i = 0; i < 3; i++) 
-	{
-		// Checking horizontal win condition
-		if ( board[i][0] === 'X' && board[i][1] === 'X' && board[i][2] === 'X')
-		{	return true;	}
-
-		else if ( board[i][0] === 'O' && board[i][1] === 'O' && board[i][2] === 'O')
-		{	return true;	}
-
-		// Checking vertical win condition
-		else if ( board[0][i] === 'X' && board[1][i] === 'X' && board[2][i] === 'X')
-		{	return true;	}
-
-		else if ( board[0][i] === 'O' && board[1][i] === 'O' && board[2][i] === 'O')
-		{	return true;	}
-	}
-
-	// Checking diagnol win conditions 
-	if (board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X')
-	{	return true;	}
-
-	if (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O')
-	{	return true;	}
-
-	if (board[2][0] === 'X' && board[1][1] === 'X' && board[0][2] === 'X')
-	{	return true;	}
-
-	if (board[2][0] === 'O' && board[1][1] === 'O' && board[0][2] === 'O')
-	{	return true;	}
-
-    return false;
-}
-
-// Delay in milliseconds
-function sleep(delay) 
-{
-    var start = new Date().getTime();
-    while (new Date().getTime() < start + delay);
 }
 
 function restart()
 {
-	turn = 0;
-	board = new Array(3);
-
-	for (var row = 0; row < 3; row++) 
-	{
-	   // Resetting board
-       board[row] = new Array(3);
-
-		for (var col = 0; col < 3; col++)
-		{
-			var id = '#' + row + col;
-			$(id).text('');
-		}
-	}
-
-	if(playerSign === 'X')
-	{
-	   playerTurn = true;
-	}
-	else if (playerSign === 'O')
-	{
-	   playerTurn = false;	
-	}
+	
 }
-
-
-
 
 function display(id)
 {
    var row = parseInt( id[0] );
    var col = parseInt( id[1] );    
 
-   // Total number of turns in a game are 9 ==> 3*3
-   if (turn < 9)
-   {
-	if (playerTurn === true)
-	  {
-	     if ( board[row][col] === undefined)
-	     {
-	        board[row][col] = playerSign;
-	        $('#'+id).text(playerSign); 
-	        // printBoard();              	
-
-			 if ( checkWin() === true)
-			 {
-			 	$("#turn").text("You Won!!");
-			 	restart();			 	
-			 }
-			 else
-			 {
-			 	playerTurn = false;
-		        turn++;                   
-		        $("#turn").text("Computer's Turn");
-	         }       
-	     }	   		
-	  }
-	  
-	  else 
-	  {
-	     if (board[row][col] === undefined)
-	     {
-	        board[row][col] = computerSign;
-	        $('#'+id).text(computerSign); 
-	        // printBoard();   
-
-	        if ( checkWin() === true)
-	        {
-	        	$("#turn").text("Computer Won!");
-	        	restart();
-	        }           	
-	        else
-	        {
-	        	playerTurn = true;
-	        	turn++;       
-	        	$("#turn").text("Your Turn!"); 
-	        }  
-	     }
-	  }  
-   }   
+   console.log(row + " " + col);
 }
