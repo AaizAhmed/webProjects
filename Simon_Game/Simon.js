0e34e6e442ecd0954d7358e55faf0da9791544ca
AAIZAHMED <aaiza2@umbc.edu>
2017-03-23 14:03:00 -0400

    drawing

---
+++ Simon_Game/Simon.js
@@ -0,0 +1,19 @@
+/*
+    Simon: This file implements an Simon sound game in which 
+           the player needs to repeat the sounds in the order
+           they are played in.
+
+    Author: Aaiz N Ahmed
+    Date: 03-23-2017
+*/
+
+var audio_one = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+audio_one.play();
+
+var audio_two = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+var audio_three = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+var audio_four = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+
+ 
+ 
+
